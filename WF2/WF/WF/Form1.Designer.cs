98bd2c81ede03e4ca0b2429e5b8a6c545ba85f35
KimGiMyoung <KimGiMyoung@users.noreply.github.com>
2019-11-22 11:49:01 +0900

    first
    Railway_panic First

---
+++ WF2/WF/WF/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WF
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button9 = new System.Windows.Forms.Button();
+			this.button10 = new System.Windows.Forms.Button();
+			this.button11 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button9
+			// 
+			this.button9.Location = new System.Drawing.Point(298, 29);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(108, 49);
+			this.button9.TabIndex = 9;
+			this.button9.Text = "다음스테이지";
+			this.button9.UseVisualStyleBackColor = true;
+			this.button9.Click += new System.EventHandler(this.button9_Click);
+			// 
+			// button10
+			// 
+			this.button10.Location = new System.Drawing.Point(440, 29);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(108, 49);
+			this.button10.TabIndex = 10;
+			this.button10.Text = "스테이지1부터";
+			this.button10.UseVisualStyleBackColor = true;
+			this.button10.Click += new System.EventHandler(this.button10_Click);
+			// 
+			// button11
+			// 
+			this.button11.Location = new System.Drawing.Point(569, 30);
+			this.button11.Name = "button11";
+			this.button11.Size = new System.Drawing.Size(108, 49);
+			this.button11.TabIndex = 11;
+			this.button11.Text = "다시도전";
+			this.button11.UseVisualStyleBackColor = true;
+			this.button11.Click += new System.EventHandler(this.button11_Click);
+			// 
+			// button8
+			// 
+			this.button8.BackgroundImage = global::WF.Properties.Resources.play;
+			this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button8.Location = new System.Drawing.Point(209, 29);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(50, 50);
+			this.button8.TabIndex = 8;
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.StartButtonClick);
+			// 
+			// button7
+			// 
+			this.button7.BackgroundImage = global::WF.Properties.Resources.Fix;
+			this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button7.Location = new System.Drawing.Point(661, 150);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(50, 50);
+			this.button7.TabIndex = 7;
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.FixButtonClick);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.pictureBox1.Image = global::WF.Properties.Resources.train2;
+			this.pictureBox1.Location = new System.Drawing.Point(41, 472);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+			this.pictureBox1.TabIndex = 6;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button6
+			// 
+			this.button6.BackgroundImage = global::WF.Properties.Resources._006;
+			this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button6.Location = new System.Drawing.Point(569, 150);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(50, 50);
+			this.button6.TabIndex = 5;
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// button5
+			// 
+			this.button5.BackgroundImage = global::WF.Properties.Resources._005;
+			this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button5.Location = new System.Drawing.Point(484, 150);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(50, 50);
+			this.button5.TabIndex = 4;
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// button4
+			// 
+			this.button4.BackgroundImage = global::WF.Properties.Resources._004;
+			this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button4.Location = new System.Drawing.Point(406, 150);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(50, 50);
+			this.button4.TabIndex = 3;
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button3
+			// 
+			this.button3.BackgroundImage = global::WF.Properties.Resources._003;
+			this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button3.Location = new System.Drawing.Point(317, 150);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(50, 50);
+			this.button3.TabIndex = 2;
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button2
+			// 
+			this.button2.BackgroundImage = global::WF.Properties.Resources._002;
+			this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button2.Location = new System.Drawing.Point(228, 150);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(50, 50);
+			this.button2.TabIndex = 1;
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button1
+			// 
+			this.button1.BackgroundImage = global::WF.Properties.Resources._001;
+			this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button1.Location = new System.Drawing.Point(151, 150);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(50, 50);
+			this.button1.TabIndex = 0;
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(184, 106);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Text = "기차가 5초 후 출발한다!";
+			this.textBox1.Size = new System.Drawing.Size(150, 21);
+			this.textBox1.TabIndex = 12;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 593);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button11);
+			this.Controls.Add(this.button10);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button10;
+		private System.Windows.Forms.Button button11;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
+
---
+++ WF2/WF/WF/Form1.cs
@@ -0,0 +1,491 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace WF
+{
+	enum MAP_SIZE
+	{
+		MAP_SIZE_X = 14,
+		MAP_SIZE_Y = 6,
+	}
+
+	enum DIR
+	{
+		NONE,
+		LEFT,
+		RIGHT,
+		UP,
+		DOWN,
+	}
+
+	enum WAY_TYPE
+	{
+		NONE,
+		WAY_1,
+		WAY_2,
+		WAY_3,
+		WAY_4,
+		WAY_5,
+		WAY_6,
+	}
+
+	public partial class Form1 : Form
+	{
+		Point _pos = new Point(0, 250);
+		Size _size = new Size(50, 50);
+
+		PictureBox[,] _arrPicBox = new PictureBox[(int)MAP_SIZE.MAP_SIZE_Y, (int)MAP_SIZE.MAP_SIZE_X];
+		WAY_TYPE[,] _arrWayType = new WAY_TYPE[(int)MAP_SIZE.MAP_SIZE_Y, (int)MAP_SIZE.MAP_SIZE_X];
+
+		Image _image;
+		WAY_TYPE _wayType;
+
+		PictureBox _train;
+		Point _trainPos = new Point(60, 265);
+		Size _trainSize1 = new Size(18, 30); //18,30
+		Size _trainSize2 = new Size(30, 18); //18,30
+		DIR _trainDir = DIR.NONE;
+
+		bool _isTrainHorizon = true;
+		Point _traingMidPoint = new Point();
+
+		bool _isCancleClick = false;
+
+		System.Windows.Forms.Timer _timer = new System.Windows.Forms.Timer();
+		System.Windows.Forms.Timer _startTimer = new System.Windows.Forms.Timer();
+		bool _isSafe = true;
+
+		int  _stage = 0;
+		bool _isStageClear = false;
+		Random rand = new Random();
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			for (int i = 0; i < (int)MAP_SIZE.MAP_SIZE_Y; i++)
+			{
+				for(int j = 0; j< (int)MAP_SIZE.MAP_SIZE_X; j++)
+				{
+					_arrPicBox[i,j] = new PictureBox();
+					_pos.X += 50;
+					_arrPicBox[i,j].Location = _pos;
+					_arrPicBox[i,j].Size = _size;
+					_arrPicBox[i,j].SizeMode = PictureBoxSizeMode.StretchImage;
+					_arrPicBox[i,j].Click += new EventHandler(SetWay);
+					_arrPicBox[i, j].BackColor = SystemColors.ActiveCaption;
+					Controls.Add(_arrPicBox[i,j]);
+					_arrWayType[i, j] = WAY_TYPE.NONE;
+				}
+
+				_pos.Y += 50;
+				_pos.X = 0;
+			}
+			
+			_train = pictureBox1;
+
+			Init();
+			_timer.Interval = 1;
+			_timer.Tick += timer_Tick;
+
+			_startTimer.Interval = 5000;
+			_startTimer.Tick += StartButtonClick;
+		}
+
+		private void Init()
+		{
+			_image = null;
+
+			for (int i = 0; i < (int)MAP_SIZE.MAP_SIZE_Y; i++)
+			{
+				for (int j = 0; j < (int)MAP_SIZE.MAP_SIZE_X; j++)
+				{
+					_arrPicBox[i, j].Image = null;
+					_arrWayType[i, j] = WAY_TYPE.NONE;
+				}
+			}
+
+			_arrPicBox[0, 0].Image = global::WF.Properties.Resources._001;
+			_arrPicBox[(int)MAP_SIZE.MAP_SIZE_Y - 1, (int)MAP_SIZE.MAP_SIZE_X - 1].Image = global::WF.Properties.Resources._001;
+			_arrWayType[0, 0] = WAY_TYPE.WAY_1;
+			_arrWayType[(int)MAP_SIZE.MAP_SIZE_Y - 1, (int)MAP_SIZE.MAP_SIZE_X - 1] = WAY_TYPE.WAY_1;
+
+			_isTrainHorizon = true;
+			_isSafe = true;
+			_train.Image = Properties.Resources.train2;
+			_traingMidPoint.X = _arrPicBox[0, 0].Location.X + 25;
+			_traingMidPoint.Y = _arrPicBox[0, 0].Location.Y + 25;
+			_train.Size = _trainSize2;
+			SetTrainPos();
+			_trainDir = DIR.RIGHT;
+
+			SetBlock();
+
+			_isStageClear = false;
+			_timer.Stop();
+			_startTimer.Start();
+		}
+
+		private void SetBlock()
+		{
+			int temp = 0;
+			while(temp < _stage)
+			{
+				int i = rand.Next(0, (int)MAP_SIZE.MAP_SIZE_Y);
+				int j = rand.Next(0, (int)MAP_SIZE.MAP_SIZE_X);
+				if(_arrPicBox[i,j].Image == null)
+				{
+					_arrPicBox[i, j].Image = Properties.Resources.Block;
+					temp++;
+				}
+			}
+		}
+
+		// 길 버튼 
+		private void button1_Click(object sender, EventArgs e)
+		{
+			_image = Properties.Resources._001;
+			_wayType = WAY_TYPE.WAY_1;
+			_isCancleClick = false;
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			_image = Properties.Resources._002;
+			_wayType = WAY_TYPE.WAY_2;
+			_isCancleClick = false;
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			_image = Properties.Resources._003;
+			_wayType = WAY_TYPE.WAY_3;
+			_isCancleClick = false;
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			_image = Properties.Resources._004;
+			_wayType = WAY_TYPE.WAY_4;
+			_isCancleClick = false;
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			_image = Properties.Resources._005;
+			_wayType = WAY_TYPE.WAY_5;
+			_isCancleClick = false;
+		}
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+			_image = Properties.Resources._006;
+			_wayType = WAY_TYPE.WAY_6;
+			_isCancleClick = false;
+		}
+
+		// 픽쳐박스에 길 이미지 삽입하기
+		private void SetWay(object sender, EventArgs e)
+		{
+			int j = (PointToClient(new Point(MousePosition.X, MousePosition.Y)).X - 50)/ 50;
+			int i = (PointToClient(new Point(MousePosition.X, MousePosition.Y)).Y - 250) / 50;
+
+			// 예외처리
+			if (i >= (int)MAP_SIZE.MAP_SIZE_Y || j >= (int)MAP_SIZE.MAP_SIZE_X) return;
+
+			if ((i == 0 && j == 0) || (i == (int)MAP_SIZE.MAP_SIZE_Y - 1 && j == (int)MAP_SIZE.MAP_SIZE_X - 1)) return;
+
+			if (_arrPicBox[i, j].Image != null && _isCancleClick == true)
+			{
+				_arrPicBox[i, j].Image = null;
+				_arrWayType[i, j] = WAY_TYPE.NONE;
+			}
+
+			if(_arrPicBox[i,j].Image == null)
+			{
+				_arrPicBox[i, j].Image = _image;
+				_arrWayType[i, j] = _wayType;
+			}
+		}
+	
+		// 망치 버튼 클릭
+		private void FixButtonClick(object sender, EventArgs e)
+		{
+			_isCancleClick = true;
+			_image = null;
+			_wayType = WAY_TYPE.NONE;
+		}
+
+		// 시작 버튼 클릭
+		private void StartButtonClick(object sender, EventArgs e)
+		{
+			if (_isSafe == false) return;
+			_timer.Start();
+			_startTimer.Stop();
+		}
+
+		void timer_Tick(object sender, EventArgs e)
+		{
+			// timer 마다 이동
+			Move(_trainDir);
+
+			// 타일들 75, 275 -> 125,  325-> 125, 125 50씩 증가
+			// 중앙 값이 타일 중앙으로 오면 방향 서치
+			// 방향 서치 후 이미지 변환
+			int j = (_traingMidPoint.X - 50) / 50;
+			int i = (_traingMidPoint.Y - 250) / 50;
+
+			if(i <0 || i> (int)MAP_SIZE.MAP_SIZE_Y -1 || j <0 || j > (int)MAP_SIZE.MAP_SIZE_X - 1)
+			{
+				_timer.Stop();
+			}
+
+			Point tempP = _arrPicBox[i, j].Location;
+			tempP.X += 25;
+			tempP.Y += 25;
+
+			if (_traingMidPoint == tempP)
+			{
+				SearchDir(_arrWayType[i, j]);
+			}
+
+			// 중앙 값이 타일 경계 값일때 다음 타일 검사
+			if(_traingMidPoint.X % 50 == 0 || _traingMidPoint.Y % 50 == 0)
+			{
+				if (_trainDir == DIR.LEFT)
+				{
+					if (j - 1 < 0)
+					{
+						textBox1.Text = "열차 사고";
+						_isSafe = false;
+						_timer.Stop();
+						return;
+					}
+					CheckNextWay(_arrWayType[i, j - 1]);
+				}
+				else if (_trainDir == DIR.UP)
+				{
+					if (i - 1 < 0)
+					{
+						textBox1.Text = "열차 사고";
+						_isSafe = false;
+						_timer.Stop();
+						return;
+					}
+					CheckNextWay(_arrWayType[i - 1, j]);
+				}
+				else CheckNextWay(_arrWayType[i, j]);
+			}
+
+			// 마지막 부분인가
+			tempP = _arrPicBox[(int)MAP_SIZE.MAP_SIZE_Y - 1, (int)MAP_SIZE.MAP_SIZE_X - 1].Location;
+			tempP.X += 25;
+			tempP.Y += 25;
+			if (_traingMidPoint == tempP)
+			{
+				textBox1.Text = "열차 도착";
+				_isStageClear = true;
+				_timer.Stop();
+			}
+
+			// 기차가 안전한가
+			if (_isSafe == false)
+			{
+				textBox1.Text = "열차 사고";
+				_timer.Stop();
+			}
+		}
+
+		private void Move(DIR dir)
+		{
+			switch (dir)
+			{
+				case DIR.NONE:
+					_isSafe = false;
+					break;
+				case DIR.LEFT:
+					_traingMidPoint.X -= 1;
+					break;
+				case DIR.RIGHT:
+					_traingMidPoint.X += 1;
+					break;
+				case DIR.UP:
+					_traingMidPoint.Y -= 1;
+					break;
+				case DIR.DOWN:
+					_traingMidPoint.Y += 1;
+					break;
+				default:
+					break;
+			}
+
+			SetTrainPos();
+		}
+
+		private void SearchDir(WAY_TYPE waytype)
+		{
+			switch (waytype)
+			{
+				case WAY_TYPE.WAY_1:
+				case WAY_TYPE.WAY_2:
+					return;
+				case WAY_TYPE.WAY_3:
+					{
+						if (_trainDir == DIR.UP) { _trainDir = DIR.RIGHT; }
+						else if (_trainDir == DIR.LEFT) { _trainDir = DIR.DOWN; }
+					}
+					break;
+				case WAY_TYPE.WAY_4:
+					{
+						if(_trainDir == DIR.RIGHT) { _trainDir = DIR.DOWN; }
+						else if(_trainDir == DIR.UP) { _trainDir = DIR.LEFT; }
+					}
+					break;
+				case WAY_TYPE.WAY_5:
+					{
+						if (_trainDir == DIR.DOWN) { _trainDir = DIR.RIGHT; }
+						else if (_trainDir == DIR.LEFT) { _trainDir = DIR.UP; }
+					}
+					break;
+				case WAY_TYPE.WAY_6:
+					{
+						if (_trainDir == DIR.RIGHT) { _trainDir = DIR.UP; }
+						else if (_trainDir == DIR.DOWN) { _trainDir = DIR.LEFT; }
+					}
+					break;
+				default:
+					break;
+			}
+
+			_isTrainHorizon = !_isTrainHorizon;
+			ChangeTrainImage();
+
+		}
+
+		private void ChangeTrainImage()
+		{
+			if(_isTrainHorizon)// 가로
+			{
+				_train.Image = Properties.Resources.train2;
+				_train.Size = _trainSize2;
+			}
+			else // 세로
+			{
+				_train.Image = Properties.Resources.train;
+				_train.Size = _trainSize1;
+			}
+
+			SetTrainPos();
+		}
+
+		private void SetTrainMidPos()
+		{
+			if(_isTrainHorizon)
+			{
+				_traingMidPoint.X = _train.Location.X + 9;
+				_traingMidPoint.Y = _train.Location.Y + 15;
+			}
+			else
+			{
+				_traingMidPoint.X = _train.Location.X + 15;
+				_traingMidPoint.Y = _train.Location.Y + 9;
+			}
+		}
+
+		private void SetTrainPos()
+		{
+			Point temp = _traingMidPoint;
+			if (_isTrainHorizon)
+			{
+				temp.X -= 15;
+				temp.Y -= 9;
+				_train.Location = temp;
+			}
+			else
+			{
+				temp.X -= 9;
+				temp.Y -= 15;
+				_train.Location = temp;
+			}
+		}
+
+		private void CheckNextWay(WAY_TYPE waytype)
+		{
+			switch (waytype)
+			{
+				case WAY_TYPE.NONE:
+					_isSafe = false;
+					break;
+				case WAY_TYPE.WAY_1:
+					{
+						if(_trainDir != DIR.LEFT && _trainDir != DIR.RIGHT ) {_isSafe = false;}
+					}
+					break;
+				case WAY_TYPE.WAY_2:
+					{
+						if (_trainDir != DIR.UP && _trainDir != DIR.DOWN) { _isSafe = false; }
+					}
+					break;
+				case WAY_TYPE.WAY_3:
+					{
+						if (_trainDir != DIR.UP && _trainDir != DIR.LEFT) { _isSafe = false; }
+					}
+					break;
+				case WAY_TYPE.WAY_4:
+					{
+						if (_trainDir != DIR.UP && _trainDir != DIR.RIGHT) { _isSafe = false; }
+					}
+					break;
+				case WAY_TYPE.WAY_5:
+					{
+						if (_trainDir != DIR.LEFT && _trainDir != DIR.DOWN) { _isSafe = false; }
+					}
+					break;
+				case WAY_TYPE.WAY_6:
+					{
+						if (_trainDir != DIR.RIGHT && _trainDir != DIR.DOWN) { _isSafe = false; }
+					}
+					break;
+				default:
+					break;
+			}
+		}
+
+		private void button9_Click(object sender, EventArgs e)
+		{
+			if (_isStageClear == false) return;
+			_stage += 2;
+			Init();
+			textBox1.Text = "스테이지 : " + ((_stage / 2)+1).ToString() + " 시작";
+		}
+
+		private void button10_Click(object sender, EventArgs e)
+		{
+			_stage = 0;
+			Init();
+			textBox1.Text = "스테이지 : 1 시작";
+		}
+
+		private void button11_Click(object sender, EventArgs e)
+		{
+			Init();
+			if(_stage == 0)
+			{
+				textBox1.Text = "스테이지 : 1 시작";
+			}
+			else
+			{
+				textBox1.Text = "스테이지 : " + (_stage / 2).ToString() + " 시작";
+			}
+		}
+	}
+}
